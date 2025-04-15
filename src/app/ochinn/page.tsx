793faf32a0b023a98cd6ef40910563d133526e23
牧幸司 <s2111273@coins.tsukuba.ac.jp>
2025-04-15 20:16:35 +0900

    test

---
+++ src/app/ochinn/page.tsx
@@ -0,0 +1,3 @@
+export default function TestPage() {
+    return <div className=" grid grid-rows-[50px_1fr_50px] items-top justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">うんぽっぽ</div>
+}
