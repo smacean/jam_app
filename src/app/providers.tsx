5cdd1a67efece5e1e0b7f9844247b61f00799f2a
smacean <kazukida30@gmail.com>
2025-08-30 13:57:55 +0900

    refact: providersのフォーマット

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,4 +1,3 @@
-
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
@@ -8,8 +7,6 @@ export default function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-    </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
 }
